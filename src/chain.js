37710536c198ec70e805c6c0e47d092b22495b00
Fabien Di Tore <fabien@ditore.ch>
2020-02-10 15:02:26 +0100

    wip on Task Chains [needs to be renamed]

--- src/chain.js
+++ src/chain.js
@@ -1,23 +1,33 @@
-import {pipe,curry,chain,map} from './core'
-import {reduce,safe_push} from './array';
-import {trace} from './debug';
-import {Task} from './functor'
+import {pipe,curry,chain,map}   from './core'
+import {reduce,safe_push}       from './array';
+import {trace}                  from './debug';
+import {Task}                   from './functor'
 
+
+/*
+  return a <Function> which create a <Task> that accumulate its argument when forked in a <List> and resolve the new <List>
+*/
 export const make_task_collector = _=> {
   let collector = [];
-
   return res => new Task((reject,resolve)=>{
     collector = safe_push(collector,res)
     resolve(collector)
   });
 }
 
-export const identity_task = _=> {
+
+// identity task is the same as identity but injected in a <Task>
+// when forked, returns the same value
+export const makeIdentityTask = _=> {
   return new Task((reject,resolve)=>{
     resolve(_)
   })
 }
 
+
+/*
+  expand a <List> of <Tasks> to add a <TaskCollector> after each one of them
+*/
 export const collect_chain_reducer = curry((collector,acc,item)=>{
   acc.push(chain(item))
 //  acc.push(map(trace('x')))
@@ -25,7 +35,7 @@ export const collect_chain_reducer = curry((collector,acc,item)=>{
   return acc;
 })
 
-// Fn -> [Fn] -> Task 
+// Fn -> [Fn] -> Task
 /*
 
   pipe(
@@ -37,5 +47,36 @@ export const collect_chain_reducer = curry((collector,acc,item)=>{
     t3, -> 3
     collect, [1,2,3]
   )
+
+  for a given <List> of <Task>; Chain them and accumulate the results in an array.
+  returns <Task>
 */
-export const collect_chain = (collector)=>(...args)=> pipe(identity_task,...reduce([],collect_chain_reducer(collector),args))
+export const collect_chain = (collector)=>(...args)=> pipe(makeIdentityTask,...reduce([],collect_chain_reducer(collector),args))
+
+/*
+  short hand to create a Task collector
+  return a function that chain and accumulate results
+*/
+export const useTaskCollector = ()=>{
+  let collector = make_task_collector();
+  return collect_chain(collector);
+
+}
+
+
+/*
+  creates a full task chain helper
+*/
+export const useTaskChainCollection = (...initial_tasks)=>{
+  if(initial_tasks.length ===0)
+    throw new Error('you have to provide at least 1 initial task')
+
+  const make_chain = useTaskCollector();
+
+  const run =  make_chain(...initial_tasks)
+  return {
+    extend: (...tasks) => (a,b) => pipe(run,chain,make_chain(...tasks))().fork(a,b),
+    run: (a,b)=>run().fork(a,b),
+    tasks:initial_tasks
+  }
+}
--- test/chain.spec.js
+++ test/chain.spec.js
@@ -1,10 +1,14 @@
 import * as C from '../src/chain'
 import * as F from '../src/functor'
-import {pipe,compose,chain} from '../src/core'
+import {pipe,compose,chain,map} from '../src/core'
+
+const makeTask  = _ => new F.Task ((reject,resolve)=>{ resolve(_) })
+const makeFailTask  = _ => new F.Task ((reject,resolve)=>{ reject(`fail ${_}`) })
+
+
 
 test ("1 in chain",(done)=>{
-  let maketask  = _ => new F.Task ((reject,resolve)=>{ resolve(_) })
-  let t1 = _=> maketask('1');
+  let t1 = _=> makeTask('1');
   const collector = C.make_task_collector()
   let task_set = C.collect_chain(collector)
   const resultFn = task_set(t1)
@@ -25,13 +29,12 @@ test ("1 in chain",(done)=>{
 
 
 test ("Type",(done)=>{
-  let maketask  = _ => new F.Task ((reject,resolve)=>{ resolve(_) })
 
 
-  let t1 = _=> maketask('1');
-  let t2 = _=> maketask('2');
-  let t3 = _=> maketask('3');
-  let t4 = _=> maketask('4');
+  let t1 = _=> makeTask('1');
+  let t2 = _=> makeTask('2');
+  let t3 = _=> makeTask('3');
+  let t4 = _=> makeTask('4');
 
 
   const collector = C.make_task_collector()
@@ -58,14 +61,13 @@ test ("Type",(done)=>{
 
 
 test ("manual chain task",(done)=>{
-  let maketask  = _ => new F.Task ((reject,resolve)=>{ resolve(_) })
 
 
-  let t1 = _=> maketask('1');
-  let t2 = _=> maketask('2');
-  let t3 = _=> maketask('3');
-  let t4 = _=> maketask('4');
-  let t5 = _=> maketask('5');
+  let t1 = _=> makeTask('1');
+  let t2 = _=> makeTask('2');
+  let t3 = _=> makeTask('3');
+  let t4 = _=> makeTask('4');
+  let t5 = _=> makeTask('5');
 
 
   const collector = C.make_task_collector()
@@ -73,9 +75,9 @@ test ("manual chain task",(done)=>{
   let task_set = C.collect_chain(collector)
 
 
-  const resultFn = pipe(C.identity_task,chain(t1),chain(collector),chain(t2),chain(collector),chain(t3),chain(collector),chain(t4),chain(collector))
+  const resultFn = pipe(C.makeIdentityTask,chain(t1),chain(collector),chain(t2),chain(collector),chain(t3),chain(collector),chain(t4),chain(collector))
 
-  const resultFn2 = pipe(C.identity_task,chain(t5),chain(collector))
+  const resultFn2 = pipe(C.makeIdentityTask,chain(t5),chain(collector))
   const final = pipe(resultFn,chain(resultFn2))
 
   final().fork(
@@ -96,14 +98,13 @@ test ("manual chain task",(done)=>{
 
 
 test ("compose a collected chain",(done)=>{
-  let maketask  = _ => new F.Task ((reject,resolve)=>{ resolve(_) })
 
 
-  let t1 = _=> maketask('1');
-  let t2 = _=> maketask('2');
-  let t3 = _=> maketask('3');
-  let t4 = _=> maketask('4');
-  let t5 = _=> maketask('5');
+  let t1 = _=> makeTask('1');
+  let t2 = _=> makeTask('2');
+  let t3 = _=> makeTask('3');
+  let t4 = _=> makeTask('4');
+  let t5 = _=> makeTask('5');
 
 
   const collector = C.make_task_collector()
@@ -126,6 +127,123 @@ test ("compose a collected chain",(done)=>{
       expect(_).toEqual(['1','2','3','4','5'])
 
       done();
+    }
+  )
+
+});
+
+test ("compose a collected chain2 , several tasks append",(done)=>{
+
+
+  let t1 = _=> makeTask('1');
+  let t2 = _=> makeTask('2');
+  let t3 = _=> makeTask('3');
+  let t4 = _=> makeTask('4');
+  let t5 = _=> makeTask('5');
+  let t6 = _=> makeTask('6');
+
+
+  const collector = C.make_task_collector()
+
+  let task_set = C.collect_chain(collector)
+
+
+  const resultFn = task_set(t1,t2,t3,t4)
+
+  const resultFn2 = pipe(resultFn,chain(task_set(t5,t6)))
+
+
+  resultFn2().fork(
+
+    console.error,
+    _=>{
+      console.log(_)
+      expect(_.length).toBe(6);
+
+      expect(_).toEqual(['1','2','3','4','5','6'])
+
+      done();
+    }
+  )
+
+});
+
+test ("shorthand",(done)=>{
+
+
+  let t1 = _=> makeTask('1');
+  let t2 = _=> makeTask('2');
+  let t3 = _=> makeTask('3');
+  let t4 = _=> makeTask('4');
+  let t5 = _=> makeTask('5');
+  let t6 = _=> makeFailTask('6');
+
+  const {extend,run} = C.useTaskChainCollection(t1,t2);
+
+
+  run(
+    console.error,
+    _=>{
+      console.log(_)
+      expect(_.length).toBe(2);
+
+      expect(_).toEqual(['1','2'])
+
+      extend(t3,t4)(console.error,_=>{
+        console.log(_)
+        expect(_.length).toBe(4)
+        done()
+      })
+    }
+  )
+
+});
+
+
+test ("failing chain",(done)=>{
+
+
+  let t1 = _=> makeTask('1');
+  let t2 = _=> makeTask('2');
+  let t6 = _=> makeFailTask('6');
+
+  const {extend,run} = C.useTaskChainCollection(t1,t6,t2);
+
+
+  run(
+    _=>{
+      done()
+    },
+    console.error
+  )
+
+});
+
+test ("COMPOSE TASK TEST",(done)=>{
+
+
+  let t1 = _=> makeTask('1');
+  let t2 = _=> makeTask('2');
+  let t3 = _=> makeTask('3');
+  let t4 = _=> makeTask('4');
+
+  const {extend,run} = C.useTaskChainCollection(t1,t2);
+
+
+  run(
+    console.error,
+    _=>{
+      console.log(_)
+      expect(_.length).toBe(2);
+
+      expect(_).toEqual(['1','2'])
+
+      extend(compose(map(x=>`${x}_${x}`),t3),t4)(console.error,_=>{
+        console.log(_)
+        expect(_.length).toBe(4)
+        expect(_[2]).toBe('3_3')
+        done()
+      })
     }
   )
 
